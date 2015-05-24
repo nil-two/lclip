5633591cf409e9d7c1e9f17da40829cd602bd09a
wara <kusabashira227@gmail.com>
2015-05-24 12:40:10 +0900

    Finish obvious implementation

--- lclip.go
+++ lclip.go
@@ -1,12 +1,28 @@
 package lclip
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Clipboard struct {
+	storage map[string]string
 }
 
 func NewClipboard(path string) (*Clipboard, error) {
-	return &Clipboard{}, nil
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	c := &Clipboard{}
+	if err = json.NewDecoder(f).Decode(&c.storage); err != nil {
+		return nil, err
+	}
+	return c, nil
 }
 
 func (c *Clipboard) Get(label string) string {
-	return "bar"
+	return c.storage[label]
 }
