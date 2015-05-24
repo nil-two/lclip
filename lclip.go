8ee7752b0f539b989bfcdf23273a8b7b249ab70d
wara <kusabashira227@gmail.com>
2015-05-25 00:02:24 +0900

    Implement feature create storage if not exists

--- lclip.go
+++ lclip.go
@@ -2,11 +2,22 @@ package lclip
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
 	"github.com/mitchellh/go-homedir"
 )
+
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func createStorageFile(path string) error {
+	empty := []byte("{}\n")
+	return ioutil.WriteFile(path, empty, 0644)
+}
 
 func DefaultPath() (string, error) {
 	h, err := homedir.Dir()
@@ -22,6 +33,11 @@ type Clipboard struct {
 }
 
 func NewClipboard(path string) (*Clipboard, error) {
+	if !exists(path) {
+		if err := createStorageFile(path); err != nil {
+			return nil, err
+		}
+	}
 	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
