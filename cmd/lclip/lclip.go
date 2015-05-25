7adc8bfc56a3e9485cc29955c825750af051fc29
wara <kusabashira227@gmail.com>
2015-05-25 11:15:32 +0900

    Implement cmd_get

--- cmd/lclip/lclip.go
+++ cmd/lclip/lclip.go
@@ -3,11 +3,41 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/gonuts/commander"
 	"github.com/kusabashira/lclip"
 	"github.com/yuya-takeyama/argf"
 )
+
+var cmd_get = &commander.Command{
+	UsageLine: "get LABEL",
+	Short:     "get text from LABEL",
+	Run: func(cmd *commander.Command, args []string) error {
+		if len(args) < 1 {
+			return fmt.Errorf("no specify LABEL")
+		}
+		label := args[0]
+
+		path, err := lclip.DefaultPath()
+		if err != nil {
+			return err
+		}
+		c, err := lclip.NewClipboard(path)
+		if err != nil {
+			return err
+		}
+
+		dst := c.Get(label)
+		if _, err = os.Stdout.Write(dst); err != nil {
+			return err
+		}
+		if _, err = os.Stdout.Write([]byte("\n")); err != nil {
+			return err
+		}
+		return nil
+	},
+}
 
 var cmd_set = &commander.Command{
 	UsageLine: "set LABEL [FILE]...",
