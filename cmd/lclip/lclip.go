fe74915b2a2a8519e712294375a48e8fb379b5d4
wara <kusabashira227@gmail.com>
2015-05-25 11:02:16 +0900

    Define cmd_set

--- cmd/lclip/lclip.go
+++ cmd/lclip/lclip.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"github.com/gonuts/commander"
+)
+
+var cmd_set = &commander.Command{
+	UsageLine: "set LABEL",
+	Short:     "set text to LABEL",
+	Run: func(cmd *commander.Command, args []string) error {
+		return nil
+	},
+}
+
 func main() {
 }
