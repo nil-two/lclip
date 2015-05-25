5e2f451e6a51785fc642c924202616e40f2cc25c
wara <kusabashira227@gmail.com>
2015-05-25 11:09:48 +0900

    Implement cmd_set

--- cmd/lclip/lclip.go
+++ cmd/lclip/lclip.go
@@ -1,14 +1,43 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+
 	"github.com/gonuts/commander"
+	"github.com/kusabashira/lclip"
+	"github.com/yuya-takeyama/argf"
 )
 
 var cmd_set = &commander.Command{
-	UsageLine: "set LABEL",
+	UsageLine: "set LABEL [FILE]...",
 	Short:     "set text to LABEL",
 	Run: func(cmd *commander.Command, args []string) error {
-		return nil
+		if len(args) < 1 {
+			return fmt.Errorf("no specify LABEL")
+		}
+		label := args[0]
+
+		path, err := lclip.DefaultPath()
+		if err != nil {
+			return err
+		}
+		c, err := lclip.NewClipboard(path)
+		if err != nil {
+			return err
+		}
+
+		r, err := argf.From(args[1:])
+		if err != nil {
+			return err
+		}
+		src, err := ioutil.ReadAll(r)
+		if err != nil {
+			return err
+		}
+
+		c.Set(label, src)
+		return c.Close()
 	},
 }
 
